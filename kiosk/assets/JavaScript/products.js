9f61fd1c8698aa9930dea7fea57baf6e5e33e227
16semka19 <fullfocussemka@gmail.com>
2024-02-13 17:07:30 +0200

    FakeStoreAPI

--- kiosk/assets/JavaScript/products.js
+++ kiosk/assets/JavaScript/products.js
@@ -1,12 +1,39 @@
+
 fetch('https://fakestoreapi.com/products')
-          .then(res => res.json())
-        .then(json => {console.log(json);
-        var div = document.getElementById('product-list');
-        div.innerHTML= `<div data-id="1" class="card product" style="width: 18rem;">
-            <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <button type="button" class="btn btn-outline-primary">Add to cart</button>
-          </div>`; // append 
-        })
+    .then(res => res.json())
+    .then(products => {
+        const productList = document.getElementById('product-list');
+        let row; 
+
+        
+        products.forEach((product, index) => {
+         
+            if (index % 3 === 0) {
+                row = document.createElement('div');
+                row.classList.add('row');
+                productList.appendChild(row);
+            }
+
+        
+            const productCard = document.createElement('div');
+            productCard.classList.add('col');
+            productCard.innerHTML = `
+                <div class="card product" style="width: 18rem;" data-id="${product.id}">
+                    <div class="card-pre-body">
+                        <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.title}</h5>
+                            <p class="card-text">${product.description}</p>
+                            <p class="card-text">$${product.price}</p>
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-outline-primary">Add to cart</button>
+                </div>
+            `;
+           
+            row.appendChild(productCard);
+        });
+    })
+    .catch(error => {
+        console.error('Помилка завантаження продуктів:', error);
+    });
