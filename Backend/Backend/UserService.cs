082d5d865758317178e9dbebc67e366a9315df2e
Flamerman <vasiagavryluk20@gmail.com>
2024-02-23 08:35:18 +0200

    Added implementation and new exceptions.
    Added implementation of UserService.
    Wrote AuthenticationException and UserNotFoundException.

---
+++ Backend/Backend/UserService.cs
@@ -0,0 +1,45 @@
+﻿using backend;
+using System;
+using System.Collections.Generic;
+
+namespace Backend
+{
+    internal class UserService : IUserService
+    {
+        List<User> IUserService.getAll()
+        {
+            List<User> users = null;
+
+            users = getAllUsersXML();
+
+            if (users == null)
+            {
+                throw new UserNotFoundException("User NOT found!");
+            }
+
+            return users;
+        }
+        User IUserService.getById(int id)
+        {
+            User user = null;
+            user = getUserByIDXML(2);
+            if (user == null)
+            {
+                throw new UserNotFoundException("User NOT found!");
+            }
+            return user;
+        }
+        void IUserService.save(User user)
+        {
+            saveUserXML(user);
+        }
+        void IUserService.deleteById(int id)
+        {
+            deleteUserByID(id);
+        }
+        void IUserService.update(int id, User user)
+        {
+            updateUser(id, user);
+        }
+    }
+}
