47a9782a44d50f62c004b7df72dcba0e08aa07a1
Benswors <bensword.2005@gmail.com>
2024-02-27 16:34:03 +0200

    order class

--- Backend/Backend/order.cs
+++ Backend/Backend/order.cs
@@ -8,5 +8,18 @@ namespace Backend
 {
     internal class Order
     {
+        public int OrderNumber { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderStatus { get; set; }
+        public List<OrderItem> OrderDetails { get; set; }
+    }
+    internal class OrderItem
+    {
+        public int DetailId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
     }
 }
