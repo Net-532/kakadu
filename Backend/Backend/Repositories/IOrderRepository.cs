162b10bfec7eeae1b7bd80fa6485fe60d116933b
Benswors <bensword.2005@gmail.com>
2024-03-24 15:33:08 +0200

    add IOrderRepostiory and methods

---
+++ Backend/Backend/Repositories/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repositories
+{
+    internal interface IOrderRepository
+    {
+
+        Order getById(int id);
+        List<Order> getAll();
+        void save(Order order);
+        void changeStatus(int id, string status);
+    }
+}
