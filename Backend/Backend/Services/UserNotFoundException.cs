3a2c4ebb652bb4d34ace49646e529f0f8034d37d
Sasha <158223182+Kushniriuk19@users.noreply.github.com>
2024-03-28 20:48:00 +0200

    fix

--- Backend/Backend/Services/UserNotFoundException.cs
+++ Backend/Backend/Services/UserNotFoundException.cs
@@ -2,7 +2,7 @@
 
 namespace Kakadu.Backend.Services
 {
-    internal class UserNotFoundException : ApplicationException
+    public class UserNotFoundException : ApplicationException
     {
         public UserNotFoundException(string message) : base(message) { }
     }
