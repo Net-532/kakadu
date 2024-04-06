2e1b66abd12b0de34562a1727696914d98533939
Flamerman <vasiagavryluk20@gmail.com>
2024-04-07 00:02:28 +0300

    Add response for getting all products

--- WebServer/WebServer.cs
+++ WebServer/WebServer.cs
@@ -1,39 +1,66 @@
-﻿using System.Net;
+﻿using Kakadu.Backend.Entities;
+using Kakadu.Backend.Repositories;
+using Kakadu.Backend.Services;
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace Kakadu.WebServer
 {
     public class WebServer
-    {   
+    {
         private static readonly Int32 port = 8085;
         private static readonly IPAddress address = IPAddress.Parse("127.0.0.1");
+        private static IProductService productService;
 
         public static void Main()
         {
             TcpListener server = null;
             try
-            { 
+            {
                 server = new TcpListener(address, port);
                 server.Start();
 
                 Console.WriteLine($"Web Server Running on {address.ToString()} on port {port}...");
-                
+
                 while (true)
                 {
+                    productService = new ProductService(new ProductRepositoryXML());
+
                     TcpClient client = server.AcceptTcpClient();
-                    
+
                     NetworkStream stream = client.GetStream();
 
-                    string response = "I am OK. Waiting for instructions";
-                    byte[] responseData = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseData, 0, responseData.Length);
+                    List<Product> products = productService.GetAll();
+
+                    if (products != null)
+                    {
+                        StringBuilder jsonBuilder = new StringBuilder();
+                        string response = "HTTP/1.1 200 OK\r\n" + "Content-Type: application/json\r\n" + "Access-Control-Allow-Origin: *\r\n\r\n" + "{\"products\": [";
+                        foreach (Product product in products)
+                        {
+                            string productJson = $"{{\"Id\": {product.Id}, \"Title\": \"{product.Title}\", \"Price\": {product.Price}, \"PhotoUrl\": \"{product.PhotoUrl}\", \"Description\": \"{product.Description}\"}},";
+                            jsonBuilder.Append(productJson);
+                        }
+                        jsonBuilder.Remove(jsonBuilder.Length - 1, 1);
+                        jsonBuilder.Append("]}");
+
+                        response += jsonBuilder.ToString();
+
+                        Console.WriteLine(response);
+
+                        byte[] responseData = Encoding.UTF8.GetBytes(response);
+                        stream.Write(responseData, 0, responseData.Length);
+                    }
 
                     stream.Close();
                     client.Close();
-                    
+
                     Console.WriteLine("Response sent");
                 }
+
             }
             catch (SocketException e)
             {
