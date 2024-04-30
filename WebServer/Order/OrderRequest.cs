bffbc3773da36668238c22291ca266b742831966
Benswors <bensword.2005@gmail.com>
2024-04-30 16:34:12 +0300

    add order classes
    add OrderRequest.cs
    add OrderRequestProcessor.cs

---
+++ WebServer/Order/OrderRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakadu.WebServer.Order
+{
+    public class OrderRequest
+    {
+        List<OrderItemRequest> items { get; set; }
+
+        public class OrderItemRequest
+        {
+            public int ProductId { get; set; }
+            public int Quantity { get; set; }
+        }
+
+    }
+}
---
+++ WebServer/Order/OrderRequestProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakadu.WebServer.Order
+{
+    public class OrderRequestProcessor
+    {
+
+        private readonly OrderService _orderService;
+
+        public OrderRequestProcessor(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public void Process(OrderRequest orderRequest)
+        {
+            var order = new Order
+            {
+                Items = orderRequest.Items.Select(item => new OrderItem
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
+
+            _orderService.save(order);
+        }
+    }
+
+}
+
