09e7ee323978ce02be1c051e2445cc0729d4d097
LiannaSitnikova <liannasitnikova@gmail.com>
2024-04-30 16:35:49 +0300

    Create ProductToJsonConverter

---
+++ WebServer/ProductToJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Kakadu.WebServer
+{
+    internal class ProductToJsonConverter
+    {
+       public string Convert(Product product)
+        {
+            StringBuilder jsonsBuilder = new StringBuilder();
+            jsonsBuilder.Append("{");
+            jsonsBuilder.Append("\"id\":").Append(product.Id).Append(",");
+            jsonsBuilder.Append("\"title\":").Append(product.Title).Append(",");
+            jsonsBuilder.Append("\"price\":").Append(product.Price.ToString(CultureInfo.InvariantCulture)).Append(",");
+            jsonsBuilder.Append("\"photoUrl\":").Append(product.PhotoUrl).Append(",");
+            jsonsBuilder.Append("\"description\":").Append(product.Description);
+            jsonsBuilder.Append("}");
+            return jsonsBuilder.ToString();
+        }
+    }
+}
