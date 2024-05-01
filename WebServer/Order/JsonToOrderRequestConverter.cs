64c1f1f98d8cec9ae8c3a3525a9b2d4d947ddc13
Misha Kinel <misha099134@gmail.com>
2024-05-01 12:45:00 +0300

    JsonToOrderRequestConverter
    ..

---
+++ WebServer/Order/JsonToOrderRequestConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+
+namespace Kakadu.WebServer.Order
+{
+    public class JsonToOrderRequestConverter
+    {
+        public OrderRequest Convert(string json)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<OrderRequest>(json);
+            }
+            catch (JsonException ex)
+            { 
+                Console.WriteLine($"Помилка десеріалізації JSON: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
