d9b9d65fc05ddc8c6517ca05a67515d202d2773c
Flamerman <60045019+Havryliuk-Vasyl@users.noreply.github.com>
2024-05-01 22:21:05 +0300

    Update WebServer.cs

--- WebServer/WebServer.cs
+++ WebServer/WebServer.cs
@@ -50,7 +50,7 @@ namespace Kakadu.WebServer
                         if (products != null)
                         {
                             StringBuilder jsonBuilder = new StringBuilder();
-                            string response = "HTTP/1.1 200 OK\r\n" + "Content-Type: application/json\r\n\" + "Access-Control-Allow-Origin: *\r\n\r\n" + "[";
+                            string response = "HTTP/1.1 200 OK\r\n" + "Content-Type: application/json\r\n" + "Access-Control-Allow-Origin: *\r\n\r\n" + "[";
                             foreach (Product product in products)
                             {
                                 string priceString = product.Price.ToString();
