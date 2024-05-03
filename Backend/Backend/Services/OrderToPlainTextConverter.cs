ab1f518c821f6c33e5e0dd8ef5cb6c19ace0fa26
Benswors <bensword.2005@gmail.com>
2024-05-03 20:59:39 +0300

    add OrderToPlainTextConverter

---
+++ Backend/Backend/Services/OrderToPlainTextConverter.cs
@@ -0,0 +1,57 @@
+﻿using Kakadu.Backend.Entities;
+using System.Text;
+using Kakadu.Backend.Repositories;
+
+
+namespace Kakadu.Backend.Services
+{
+    public class OrderToPlainTextConverter
+    {
+        private readonly IProductRepository _productRepository;
+       
+
+        public OrderToPlainTextConverter(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+           
+        }
+        public static string Convert(Order order, IProductRepository productRepository)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("           Kakadu");
+            sb.AppendLine("Адреса: вул. Павла Каспрука 2");
+            sb.AppendLine();
+            sb.AppendLine("            Чек");
+            sb.AppendLine("********************************");
+            sb.AppendLine($"Дата: {order.OrderDate:dd.MM.yyyy}    Дод.iнф: txt");
+            sb.AppendLine($"Час: {order.OrderDate:HH:mm}          Дод.iнф: txt");
+            sb.AppendLine("********************************");
+
+            foreach (var item in order.Items)
+            {
+                int maxWidth = 28; 
+                Product product = productRepository.GetById(item.ProductId);
+                string title = product != null ? product.Title : $" {item.ProductId}";
+
+                string[] titleParts = title.Split(' ');
+
+                foreach (string part in titleParts)
+                {
+                    int padding = maxWidth - part.Length - item.Price.ToString("0.00").Length;
+                    string spaces = new string(' ', padding);
+                    sb.AppendLine($"{part}{spaces}{item.Price:0.00} грн");
+                }
+            }
+
+            sb.AppendLine("--------------------------------");
+            sb.AppendLine($"Сума                  {order.TotalPrice:0.00} грн");
+            sb.AppendLine("--------------------------------");
+            sb.AppendLine("       Дякуємо за покупку!");
+
+            return sb.ToString();
+        }
+
+    }
+    
+}
