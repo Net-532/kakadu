8244bc47a660e15a2fabb93732a50c840877cad7
LiannaSitnikova <liannasitnikova@gmail.com>
2024-05-08 09:43:47 +0300

    Update ProductToJsonConverter.cs

--- WebServer/ProductToJsonConverter.cs
+++ WebServer/ProductToJsonConverter.cs
@@ -1,17 +1,41 @@
-﻿namespace Kakadu.WebServer
+﻿using Kakadu.Backend.Entities;
+using System.Globalization;
+using System.Text;
+
+namespace Kakadu.WebServer
 {
     internal class ProductToJsonConverter
     {
-       public string Convert(Product product)
+        public string Convert(Product product)
         {
             StringBuilder jsonsBuilder = new StringBuilder();
             jsonsBuilder.Append("{");
             jsonsBuilder.Append("\"id\":").Append(product.Id).Append(",");
-            jsonsBuilder.Append("\"title\":").Append(product.Title).Append(",");
-            jsonsBuilder.Append("\"price\":").Append(product.Price.ToString(CultureInfo.InvariantCulture)).Append(",");
-            jsonsBuilder.Append("\"photoUrl\":").Append(product.PhotoUrl).Append(",");
-            jsonsBuilder.Append("\"description\":").Append(product.Description);
+            jsonsBuilder.Append("\"title\":").Append("\"").Append(product.Title).Append("\"").Append(",");
+            jsonsBuilder.Append("\"price\":").Append("\"").Append(product.Price.ToString(CultureInfo.InvariantCulture)).Append("\"").Append(",");
+            jsonsBuilder.Append("\"photoUrl\":").Append("\"").Append(product.PhotoUrl).Append("\"").Append(",");
+            jsonsBuilder.Append("\"description\":").Append("\"").Append(product.Description);
             jsonsBuilder.Append("}");
+            return jsonsBuilder.ToString();
+        }
+
+        public string Convert(List<Product> products)
+        {
+            StringBuilder jsonsBuilder = new StringBuilder();
+            jsonsBuilder.Append("[");
+
+            foreach (Product product in products)
+            {
+                jsonsBuilder.Append(Convert(product));
+                jsonsBuilder.Append(",");
+            }
+
+            if (products.Count > 0)
+            {
+                jsonsBuilder.Length--;
+            }
+
+            jsonsBuilder.Append("]");
             return jsonsBuilder.ToString();
         }
     }
