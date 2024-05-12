5bbba32c19e87b5382f7f276d687075c0862bf18
Benswors <bensword.2005@gmail.com>
2024-05-12 17:51:23 +0300

    update OrderToPlainTextConverter.cs

--- Backend/Backend/Services/OrderToPlainTextConverter.cs
+++ Backend/Backend/Services/OrderToPlainTextConverter.cs
@@ -15,27 +15,28 @@ namespace Kakadu.Backend.Services
             _productService = productService;
         }
 
-        public string Convert(Order order, IProductService productService)
+        public string Convert(Order order)
         {
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("           Kakadu");
             sb.AppendLine("Адреса: вул. Павла Каспрука 2");
             sb.AppendLine();
-            sb.AppendLine($"            Чек №: {order.Id}");
+            sb.AppendLine($"            Чек №: {order.OrderNumber}");
             sb.AppendLine("********************************");
             sb.AppendLine($"Дата: {order.OrderDate:dd.MM.yyyy}");
-            sb.AppendLine($"Час: {order.OrderDate:HH:mm}");
+            sb.AppendLine($"Час: {order.OrderDate:HH:mm:ss}");
             sb.AppendLine("********************************");
 
             int maxWidth = 28;
             foreach (var item in order.Items)
             {
-                Product product = productService.GetById(item.ProductId);
+                Product product = _productService.GetById(item.ProductId);
                 string title = product.Title;
+                int quantity = item.Quantity;
                 int padding = maxWidth - title.Length - item.Price.ToString("0.00").Length;
                 string spaces = new string(' ', padding);
-                sb.AppendLine($"{title}{spaces}{item.Price:0.00}");
+                sb.AppendLine($"{title}{spaces}{quantity} x {item.Price:0.00}");
             }
 
             sb.AppendLine("--------------------------------");
