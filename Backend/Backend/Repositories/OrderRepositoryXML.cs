87feae3d8a64e1bf0f318f60ee8af610f8f0d4ca
Stas0321 <stas03022006@gmail.com>
2024-05-14 15:28:04 +0300

    Done UpdatedAt

--- Backend/Backend/Repositories/OrderRepositoryXML.cs
+++ Backend/Backend/Repositories/OrderRepositoryXML.cs
@@ -150,7 +150,7 @@ namespace Kakadu.Backend.Repositories
             if (node != null)
             {
                 node.SelectSingleNode("Status").InnerText = status;
-                node.SelectSingleNode("UpdateAT").InnerText = DateTime.Now.ToString("s", CultureInfo.InvariantCulture); ;
+                node.SelectSingleNode("UpdatedAt").InnerText = DateTime.Now.ToString("s", CultureInfo.InvariantCulture);
                 doc.Save(filePath);
             }
         }
@@ -162,6 +162,7 @@ namespace Kakadu.Backend.Repositories
             order.Id = int.Parse(node.SelectSingleNode("Id").InnerText);
             order.TotalPrice = decimal.Parse(node.SelectSingleNode("TotalPrice").InnerText, CultureInfo.InvariantCulture);
             order.OrderDate = DateTime.Parse(node.SelectSingleNode("OrderDate").InnerText);
+            order.UpdatedAt = DateTime.Parse(node.SelectSingleNode("UpdatedAt").InnerText);
             order.Status = node.SelectSingleNode("Status").InnerText;
 
 
