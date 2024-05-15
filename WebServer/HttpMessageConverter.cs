cbf7f6da0bcacab1eb55fdefaeefa83b59d04cf4
Flamerman <vasiagavryluk20@gmail.com>
2024-05-15 20:20:12 +0300

    Remarks

--- WebServer/HttpMessageConverter.cs
+++ WebServer/HttpMessageConverter.cs
@@ -17,43 +17,49 @@
 
             string[] requestLine = lines[0].Split(' ');
 
-            switch (requestLine[0])
-            {
-                case "GET":
-                    request.Method = HttpMethod.GET;
-                    break;
-                case "POST":
-                    request.Method = HttpMethod.POST;
-                    break;
-                case "PUT":
-                    request.Method = HttpMethod.PUT;
-                    break;
-                default:
-                    throw new ArgumentException("Невідомий метод запиту");
-            }
-
             string[] requestPathAndValues = requestLine[1].Split('?');
             request.RootPath = requestPathAndValues[0];
 
-            string[] valueSplit = requestPathAndValues[1].Split('=');
-            string value = valueSplit[1];
-
-            request.Parameters["orderId"] = value;
-
-            int emptyLineIndex = Array.IndexOf(lines, "");
-
-            if (emptyLineIndex != -1 && emptyLineIndex < lines.Length - 1)
+            if (requestPathAndValues.Length > 1)
             {
-                for (int i = 1; i < emptyLineIndex; i++)
+
+                switch (requestLine[0])
                 {
-                    string[] header = lines[i].Split(new[] { ": " }, StringSplitOptions.None);
-                    request.Headers.Add(header[0], header[1]);
+                    case "GET":
+                        request.Method = HttpMethod.GET;
+                        break;
+                    case "POST":
+                        request.Method = HttpMethod.POST;
+                        break;
+                    case "PUT":
+                        request.Method = HttpMethod.PUT;
+                        break;
+                    default:
+                        throw new ArgumentException("Невідомий метод запиту");
                 }
 
-                request.Body = string.Join("\n", lines, emptyLineIndex + 1, lines.Length - emptyLineIndex - 1);
+                string[] valueSplit = requestPathAndValues[1].Split('=');
+                string value = valueSplit[1];
+
+                request.Parameters[$"{valueSplit[0]}"] = value;
+
+                int emptyLineIndex = Array.IndexOf(lines, "");
+
+                if (emptyLineIndex != -1 && emptyLineIndex < lines.Length - 1)
+                {
+                    for (int i = 1; i < emptyLineIndex; i++)
+                    {
+                        string[] header = lines[i].Split(new[] { ": " }, StringSplitOptions.None);
+                        request.Headers.Add(header[0], header[1]);
+                    }
+
+                    request.Body = string.Join("\n", lines, emptyLineIndex + 1, lines.Length - emptyLineIndex - 1);
+                }
+
+                return request;
             }
 
-            return request;
+            return null;
         }
     }
 }
