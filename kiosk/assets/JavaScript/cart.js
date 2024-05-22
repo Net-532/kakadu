a66636b44b1dbbfd3f4c4efa5cb0ef5de28db141
16semka19 <fullfocussemka@gmail.com>
2024-05-22 17:14:24 +0300

    fix check

--- kiosk/assets/JavaScript/cart.js
+++ kiosk/assets/JavaScript/cart.js
@@ -2,9 +2,9 @@ let checkTab = true;
 let order = null;
 
 function createCartItemElement(item) {
-    const itemElement = document.createElement('div');
-    itemElement.setAttribute('id', 'cart-item-full');
-    itemElement.innerHTML = `
+  const itemElement = document.createElement("div");
+  itemElement.setAttribute("id", "cart-item-full");
+  itemElement.innerHTML = `
         <img src="${item.photoUrl}" id="cart-item-img">
         <div id="cart-item-text">
             <div id="cart-text-bold">${item.title}</div>
@@ -19,167 +19,180 @@ function createCartItemElement(item) {
         <button class="remove-button btn btn-close mb-3" id="cart-remove-button"></button>
     `;
 
-    const incrementButton = itemElement.querySelector('.increment-button');
-    incrementButton.addEventListener('click', function(event) {
-        addToCart(item);
-        renderCart();
-    });
+  const incrementButton = itemElement.querySelector(".increment-button");
+  incrementButton.addEventListener("click", function (event) {
+    addToCart(item);
+    renderCart();
+  });
 
-    const decrementButton = itemElement.querySelector('.decrement-button');
-    decrementButton.addEventListener('click', function(event) {
-        removeFromCart(item);
-        renderCart();
-    });
+  const decrementButton = itemElement.querySelector(".decrement-button");
+  decrementButton.addEventListener("click", function (event) {
+    removeFromCart(item);
+    renderCart();
+  });
 
-    const removeButton = itemElement.querySelector('.remove-button');
-    removeButton.addEventListener('click', function(event) {
-        removeItemFromCart(item);
-    });
+  const removeButton = itemElement.querySelector(".remove-button");
+  removeButton.addEventListener("click", function (event) {
+    removeItemFromCart(item);
+  });
 
-    return itemElement;
+  return itemElement;
 }
 
 function addToCart(product) {
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    let existingProductIndex = cartItems.findIndex(item => item.id === product.id);
-    if (existingProductIndex !== -1) {
-        cartItems[existingProductIndex].quantity = (cartItems[existingProductIndex].quantity || 1) + 1;
-    } else {
-        product.quantity = 1;
-        cartItems.push(product);
-    }
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  let existingProductIndex = cartItems.findIndex(
+    (item) => item.id === product.id
+  );
+  if (existingProductIndex !== -1) {
+    cartItems[existingProductIndex].quantity =
+      (cartItems[existingProductIndex].quantity || 1) + 1;
+  } else {
+    product.quantity = 1;
+    cartItems.push(product);
+  }
 
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    displayAlert("success", product.title, "додано в кошик");
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  displayAlert("success", product.title, "додано в кошик");
 }
 
 function removeFromCart(product) {
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    let existingProductIndex = cartItems.findIndex(item => item.id === product.id);
-    if (existingProductIndex !== -1) {
-        cartItems[existingProductIndex].quantity -= 1;
-        if (cartItems[existingProductIndex].quantity === 0) {
-            cartItems.splice(existingProductIndex, 1);
-        }
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  let existingProductIndex = cartItems.findIndex(
+    (item) => item.id === product.id
+  );
+  if (existingProductIndex !== -1) {
+    cartItems[existingProductIndex].quantity -= 1;
+    if (cartItems[existingProductIndex].quantity === 0) {
+      cartItems.splice(existingProductIndex, 1);
     }
+  }
 
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    renderCart();
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  renderCart();
 }
 
 function removeItemFromCart(product) {
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    let existingProductIndex = cartItems.findIndex(item => item.id === product.id);
-    if (existingProductIndex !== -1) {
-        cartItems.splice(existingProductIndex, 1);
-    }
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  let existingProductIndex = cartItems.findIndex(
+    (item) => item.id === product.id
+  );
+  if (existingProductIndex !== -1) {
+    cartItems.splice(existingProductIndex, 1);
+  }
 
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    renderCart();
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  renderCart();
 }
 
 function calculateTotalSum() {
-    let totalSum = 0;
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    cartItems.forEach(item => {
-        totalSum += item.price * item.quantity;
-    });
-    return totalSum;
+  let totalSum = 0;
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  cartItems.forEach((item) => {
+    totalSum += item.price * item.quantity;
+  });
+  return totalSum;
 }
 
 function renderCart() {
-    const offcanvasBody = document.querySelector('#offcanvasBottom .offcanvas-body');
-    offcanvasBody.innerHTML = '';
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    cartItems.forEach(item => {
-        const itemElement = createCartItemElement(item);
-        offcanvasBody.appendChild(itemElement);
-    });
+  const offcanvasBody = document.querySelector(
+    "#offcanvasBottom .offcanvas-body"
+  );
+  offcanvasBody.innerHTML = "";
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  cartItems.forEach((item) => {
+    const itemElement = createCartItemElement(item);
+    offcanvasBody.appendChild(itemElement);
+  });
 
-    if (cartItems.length === 0 && checkTab == true) {
-        const emptyCart = document.createElement('div');
-        emptyCart.textContent = 'Кошик порожній';
-        offcanvasBody.appendChild(emptyCart);
-    }
+  if (cartItems.length === 0 && checkTab == true) {
+    const emptyCart = document.createElement("div");
+    emptyCart.textContent = "Кошик порожній";
+    offcanvasBody.appendChild(emptyCart);
+  }
 
-    const totalSumElements = document.querySelectorAll('.cart-numb');
-    totalSumElements.forEach(element => {
-        const totalSum = calculateTotalSum();
-        element.textContent = `${totalSum} грн`;
-    });
-}
+  const totalSumElements = document.querySelectorAll(".cart-numb");
+  totalSumElements.forEach((element) => {
+    const totalSum = calculateTotalSum();
+    element.textContent = `${totalSum} грн`;
+  });
 
-   
-function clearCart() {
-    localStorage.removeItem('cartItems');
+  const clearCartButton = document.getElementById("cart-clear-button");
+  clearCartButton.addEventListener("click", function (event) {
+    localStorage.removeItem("cartItems");
     renderCart();
-}
+  });
 
-function checkoutOrder() {
-    const items = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const printCheck = document.getElementById("cart-button-check");
+  printCheck.addEventListener("click", function (event) {
+    fetch(`${printReceiptEndpoint}?orderId=${order.id}`, {
+      method: "PUT",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Чек надруковано!");
+        const cartBottom = document.getElementById("cart-bottom");
+        cartBottom.style.display = "none";
+        const cartCheck = document.getElementById("cart-button-check");
+        cartCheck.style.display = "block";
+        checkTab = true;
+        renderCart();
+      })
+      .catch((error) => {
+        console.error("Помилка: ", error);
+      });
+  });
+
+  const checkoutButton = document.getElementById("cart-button-order");
+  checkoutButton.addEventListener("click", function (event) {
+    const items = JSON.parse(localStorage.getItem("cartItems")) || [];
     const sendRequest = {
-        items: items.map(item => ({
-            productId: item.id,
-            quantity: item.quantity
-        }))
+      items: items.map((item) => ({
+        productId: item.id,
+        quantity: item.quantity,
+      })),
     };
     fetch(ordersEndpoint, {
-        method: 'POST',
-        body: JSON.stringify(sendRequest)
+      method: "POST",
+      body: JSON.stringify(sendRequest),
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Замовлення виконано');
-	 DisplayCheckTab();
-         order = data;
-         clearCart();
-         renderReceipt(data);
-    })
-    .catch(error => {
-        console.error('Помилка: ', error);
-    });
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Замовлення виконано", data);
+        // data
+        //             {
+        //     "orderNumber": 8,
+        //     "id": "108",
+        //     "orderDate": "21.05.2024",
+        //     "orderTime": "14:13:35",
+        //     "totalPrice": 65.00,
+        //     "items": [
+        //         {
+        //             "title": "Молочний коктейль",
+        //             "quantity": 1,
+        //             "price": 65.00,
+        //             "amount": 65.00
+        //         }
+        //     ]
+        // }
 
-const printCheck = document.getElementById('cart-button-check');
-printCheck.addEventListener('click', function(event) {
-    fetch(`${printReceiptEndpoint}?orderId=${order.id}`, {
-        method: 'PUT',
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Чек надруковано!');
-        localStorage.removeItem('cartItems');
-        DisplayOrderTab();
+        localStorage.removeItem("cartItems");
+        const cartBottom = document.getElementById("cart-bottom");
+        cartBottom.style.display = "none";
+        const cartCheck = document.getElementById("cart-button-check");
+        cartCheck.style.display = "block";
+        checkTab = false;
+        order = data;
         renderCart();
-    })
-    .catch(error => {
-        console.error('Помилка: ', error);
-    });
-});
-
-function DisplayCheckTab()
-{
-    const cartBottom = document.getElementById('cart-bottom');
-    cartBottom.style.display = 'none';
-    const cartCheck = document.getElementById('cart-button-check');
-    cartCheck.style.display = 'block';
-    checkTab = false;
-}
-
-function DisplayOrderTab()
-{
-    const cartBottom = document.getElementById('cart-bottom');
-    cartBottom.style.display = 'block';
-    const cartCheck = document.getElementById('cart-button-check');
-    cartCheck.style.display = 'none';
-    checkTab = true;
+        renderReceipt(data);
+      })
+      .catch((error) => {
+        console.error("Помилка: ", error);
+      });
+  });
 }
 
 function renderReceipt(order) {
-  console.log(order);
-  const currentDate = new Date().toLocaleDateString("uk-UA");
-  const currentTime = new Date().toLocaleTimeString("uk-UA");
-
   const header = `
         <center>
         <p>Kakadu</p>
@@ -187,27 +200,28 @@ function renderReceipt(order) {
         <p>Чек # ${order.orderNumber}</p>
         <hr>
         <div class="container">
-        <div class="name"><p>Дата:${currentDate}</p>
-        <p>Час:${currentTime}</p></div>
+        <div class="name"><p>Дата:${order.orderDate}</p>
+        <p>Час:${order.orderTime}</p></div>
         </div>
         <hr>
         `;
   let body = ``;
-  let totalSum = 0;
   order.items.forEach((item) => {
     body += `
     <center>
-    <div class="container">
+    <div class=".container-receipt">
                       <div class="name">${item.title}</div>
                      <div class="price"> ${item.quantity}x${item.price}</div>
             </div>
             `;
-    totalSum += item.price * item.quantity;
   });
   const footer = `
   <center>
         <hr>
-        <div id="totalSum"></div>
+        <div class=".container-receipt">
+  <div class="name">Сума:</div> 
+  <div class="price">${order.totalPrice} грн</div>
+    </div>
         <hr>
         <p>Дякуємо за покупку!</p>
     `;
@@ -216,25 +230,18 @@ function renderReceipt(order) {
   itemElement.classList.add("receipt");
   itemElement.innerHTML = header + body + footer;
   check.appendChild(itemElement);
-  const totalSumElement = document.getElementById("totalSum");
-  totalSumElement.innerHTML = `
-    <div class="container">
-  <div class="name">Сума:</div> 
-  <div class="price">${totalSum} грн</div>
-    </div>
-    `;
 }
 
 function displayAlert(type, item, message) {
-    let alert = `
+  let alert = `
         <div class="alert alert-${type} alert-dismissible" id="add-alert" role="alert">
             <div>${item} ${message}</div>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     `;
 
-    let alertContainer = document.getElementById('alert-container');
-    if (alertContainer) {
-        alertContainer.innerHTML = alert;
-    } 
+  let alertContainer = document.getElementById("alert-container");
+  if (alertContainer) {
+    alertContainer.innerHTML = alert;
+  }
 }
