67b5e0a99e8198d814ed4f0b7acc434264b769bf
Sasha <158223182+Kushniriuk19@users.noreply.github.com>
2024-05-29 16:40:24 +0300

    fix

--- Backend/Backend/Services/OrderNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Kakadu.Backend.Services
-{
-    public class EntityNotFoundException : ApplicationException
-    {
-        public EntityNotFoundException(string message) : base(message) { }
-    }
-}
