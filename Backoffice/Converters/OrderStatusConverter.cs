df4f79ef7306f44577efa4cc0c977b072bfa61e4
Zombak1322 <zombakzt228@gmail.com>
2024-06-08 17:47:53 +0300

    added converter and color to status

---
+++ Backoffice/Converters/OrderStatusConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Backoffice.Converters
+{        public class OrderStatusConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                string status = value as string;
+                if (status != null)
+                {
+                    switch (status)
+                    {
+                        case "Done":
+                            return "Готове";
+                        case "Processing":
+                            return "Виконується";
+                        default:
+                            return status;
+                    }
+                }
+                return null;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return null;
+            }
+        }
+}
