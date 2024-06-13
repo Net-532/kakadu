11a0083ce38e924ae27221871cf01a43f03ace12
16semka19 <fullfocussemka@gmail.com>
2024-06-13 16:44:35 +0300

    dont commit

--- Backoffice/Views/MainWindow.xaml.cs
+++ Backoffice/Views/MainWindow.xaml.cs
@@ -5,29 +5,58 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Controls;
 using Backoffice.Views;
+using System.Windows.Media.Imaging;
 
 namespace Kakadu.Backoffice.Views
 {
     public partial class MainWindow : Window
     {
+        private string _username;
+        private string _userImagePath;
 
-        public MainWindow()
+        public MainWindow(string username, string userImagePath)
         {
             InitializeComponent();
+            _username = username;
+            _userImagePath = userImagePath;
+            GreetUser();
+        }
+
+        private void GreetUser()
+        {
+            GreetingTextBlock.Text = $"Привіт, {_username}";
+
+
+            
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(_userImagePath, UriKind.RelativeOrAbsolute);
+                bitmap.EndInit();
+              
+            
+            GreetingPanel.Visibility = Visibility.Visible;
+        }
+
+        private void HideGreeting()
+        {
+            GreetingPanel.Visibility = Visibility.Collapsed;
         }
 
         private void ProductButton_Click(object sender, RoutedEventArgs e)
         {
+            HideGreeting();
             MainFrame.Content = new Products();
         }
 
         private void OrderButton_Click(object sender, RoutedEventArgs e)
         {
+            HideGreeting();
             MainFrame.Content = new Orders();
         }
 
         private void UserButton_Click(object sender, RoutedEventArgs e)
         {
+            HideGreeting();
             MainFrame.Content = new Users();
         }
 
